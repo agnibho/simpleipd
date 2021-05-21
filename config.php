d4aad023fe2fbb86e46196a5199905435bd35e93
Agnibho Mondal <mondal@agnibho.com>
2021-05-21 12:28:54 +0530

    minor

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("CONFIG_URL", "");
-define("CONFIG_ROOT", __DIR__."/");
-define("CONFIG_WWW", CONFIG_ROOT."www/");
-define("CONFIG_LIB", CONFIG_ROOT."lib/");
-define("CONFIG_DB", CONFIG_ROOT."database/data.db");
-define("CONFIG_LOG", CONFIG_ROOT."database/log.db");
-define("CONFIG_TITLE", "SimpleIPD");
-?>
